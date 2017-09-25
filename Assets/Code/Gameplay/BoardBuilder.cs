06dd5031d48cb43235afe746c2651cdaa5684561
crener <martin661070@gmail.com>
2017-09-25 03:31:06 +0100

    Added Board builder code and simple square tile prefab

---
+++ Assets/Code/Gameplay/BoardBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Code.Gameplay;
+using Code.Menu;
+using UnityEngine;
+
+public class BoardBuilder : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject SquarePrefab;
+    [SerializeField, Range(0, 1)]
+    private float boardSpacer = 0.9f;
+
+    private RectTransform trans;
+
+    void Start()
+    {
+        trans = gameObject.GetComponent<RectTransform>();
+        GameOption opt = GetComponentInParent<GameOption>();
+        SizeSelector.GameSize option = opt.Option;
+
+
+        if(false)
+        {
+            //load the previous state
+        }
+        else
+        {
+            //create a new game board
+            BuildBoard(option.X, option.Y);
+        }
+    }
+
+    private void BuildBoard(int boardX, int boardY)
+    {
+        Vector2 squareSize = new Vector2
+        {
+            x = trans.rect.size.x / boardX,
+            y = trans.rect.size.y / boardY
+        };
+
+        Vector2 topLeft = new Vector2(trans.position.x, trans.position.y) - (trans.rect.size / 2);
+        topLeft += squareSize / 2; //add half square size here once rather than X*Y times within the loop
+
+        for(int y = 0; y < boardY; y++)
+        {
+            for(int x = 0; x < boardX; x++)
+            {
+                Vector2 squarePos = new Vector2
+                {
+                    x = topLeft.x + (squareSize.x * x),
+                    y = topLeft.y + (squareSize.y * y)
+                };
+
+                GameObject newSquare = Instantiate(SquarePrefab, squarePos, new Quaternion(), trans);
+                newSquare.name = x + " " + y;
+                
+                RectTransform squareTrans = newSquare.GetComponent<RectTransform>();
+                squareTrans.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, squareSize.x * boardSpacer);
+                squareTrans.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, squareSize.y * boardSpacer);
+            }
+        }
+    }
+
+//    private void OnDrawGizmos()
+//    {
+//        {
+//            Vector2 topLeft = new Vector2(trans.position.x, trans.position.y);
+//
+//            Debug.DrawLine(topLeft + (Vector2.down * 4), topLeft + (Vector2.up * 4), Color.red);
+//            Debug.DrawLine(topLeft + (Vector2.left * 4), topLeft + (Vector2.right * 4), Color.red);
+//        }
+//        {
+//            Vector2 topLeft = new Vector2(trans.position.x, trans.position.y);
+//            topLeft += new Vector2(-Mathf.Abs(trans.rect.size.x / 2), Mathf.Abs(trans.rect.size.y / 2));
+//
+//            Debug.DrawLine(topLeft + (Vector2.down * 4), topLeft + (Vector2.up * 4), Color.blue);
+//            Debug.DrawLine(topLeft + (Vector2.left * 4), topLeft + (Vector2.right * 4), Color.blue);
+//        }
+//    }
+}
